4d7171b47f0e8ab5629c0941358a36956ddc5073
Laura Moser <lauralynn@fullsail.edu>
2015-03-06 00:09:49 -0600

    Added more to the story

--- Madlibs/main.py
+++ Madlibs/main.py
@@ -67,22 +67,36 @@ elif gender == "Female":
 
 
 madlib = '''
-Once upon a time, in a kingdom far away from her own land {pronoun}, awoke with a daze. {pronoun} looked up and
-didn't recognize where {pronoun} was at, it was a strange land! After walking for {minutes} minutes, {pronoun} came upon
-a dirt road and saw a horse and carriage. {pronoun} waved to get this persons attention.
+Once upon a time, in a kingdom far away from her own land, {first_name} awoke with a daze. {pronoun} looked up and
+didn't recognize where {pronoun} was at, it was a strange land.
+
+After walking for approximately {minutes} minutes, {pronoun} came upon a dirt road and saw a horse and carriage.
+{pronoun} waved to get this persons attention.
 
 {friend_name} slowed to a stop and said "What is your name {pronoun2}!?"
 
 {first_name} replied "My name is {first_name}, I am lost - can you help me?"
 
 {friend_name} answered "There is a local tavern north, I am headed that way I will drop you off." {friend_name} not wanting
- any trouble said "That is the best I can offer you."
+any trouble said "That is the best I can offer you."
 
 {first_name} replied "Thank you so much! Can you tell me where I am at, what land is this?"
 
 {friend_name} answered "The Enchanted Forest of course!, What land are you from?"
 
-{friend_name} had climbed on to the carriage, and quickly replied "The ENCHANTED FOREST! I need to find some {travel}
+{first_name} had climbed on to the carriage, and quickly replied "The ENCHANTED FOREST! I need to find some {travel[opt1]}, {travel[opt2]}, or {travel[opt3]}!"
+
+{friend_name} looked at his riding companion strangely, and knew his first instincts were right. Something was not right with this person. He wanted to tell the
+Evil Queen at once, so as to maybe get a reward. He hurried on to the local tavern, and they rode in silence.
+
+{first_name} realized was not in {city} anymore and would have to relay on her fairytale knowledge, if she wanted to arrive home safely!
+
+After what seemed like hours, they finally arrived at the tavern. {friend_name} said "This is where I leave you. I'm sorry but I must be going. I wish you safe travels, hope you find your way."
+
+{first_name} said "Thank you my friend, I understand."
+
+{first_name} stepped into the tavern, and couldn't believe what she say. Never before had she seen real live {creatures} and other mystical creatures before.
+
 
 
 '''
