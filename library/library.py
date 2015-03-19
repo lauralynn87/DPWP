8eb5a5aaa61fb334e0ce57efca17454fe3665726
Laura Moser <lauralynn@fullsail.edu>
2015-03-19 13:58:33 -0500

    added comments in header

---
+++ library/library.py
@@ -0,0 +1,6 @@
+'''
+Name: Laura Moser
+Class: Design Patterns for Web Programming
+Assignment: Reusable Library (Week 3)
+Date: 03/18/15
+'''
--- library/pages.py
+++ library/pages.py
@@ -1,3 +1,10 @@
+'''
+Name: Laura Moser
+Class: Design Patterns for Web Programming
+Assignment: Reusable Library (Week 3)
+Date: 03/18/15
+'''
+
 class Form():
     def __init__(self):
         self.__title = "Welcome!"
