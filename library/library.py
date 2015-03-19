d3787ba34f3e22c90cdb26bd45b45b0f1149c931
Laura Moser <lauralynn@fullsail.edu>
2015-03-19 15:33:06 -0500

    Added math functions

--- library/library.py
+++ library/library.py
@@ -5,14 +5,19 @@ Assignment: Reusable Library (Week 3)
 Date: 03/18/15
 '''
 
-class unknownTitle(object):
+class TvSize(object):
+    def __init__(self):
     pass
 
     def min_size(self):
-        pass
+        #divide data by 3
+        min = distance / 3
+        return value min()
 
     def max_size(self):
-        pass
+        #divide data by 1.5
+        max = distance / 1.5
+        return value max()
 
     def reso(self):
         pass
