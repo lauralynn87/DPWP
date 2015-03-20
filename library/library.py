ae849e6c3121f66288e7b3101d2f905a41f2c918
Laura Moser <lauralynn@fullsail.edu>
2015-03-19 23:55:09 -0500

    Created Getters/Setters for distance and resolution

--- library/library.py
+++ library/library.py
@@ -8,8 +8,8 @@ Date: 03/18/15
 class TvSize(object):
     def __init__(self):
         #define variables // will need to make private before turning in.
-        self.distance = 0
-        self.resolution = ""
+        self._distance = 0
+        self._resolution = ""
 
     def min_size(self):
         #divide user data by 3
@@ -24,9 +24,31 @@ class TvSize(object):
     def reso(self):
         # First checks to see if user data is less than 42 inches - if so will print message.
         # Than checks to see what resolution user selected if they selected 4K Ultra than the message 'You have chosen the recommended resolution'
-        if distance < 42 and resolution == "480p" or "720p" or "1080p":
+        if (distance < 42 and (resolution == "480p" or "720p" or "1080p")):
              print ("You need to upgrade to 4K Ultra HD for the best Experience!")
         elif resolution == "4K Ultra":
             print ("You've chosen the correct Resolution for your viewing distance!")
         elif distance > 42:
             print ("The Resolution you've chosen is fine for your viewing distance.")
+
+    #Getter
+    @property
+    def distance(self):
+        return self._distance
+
+    #Setter
+    @distance.setter
+    def distance(self, new_distance):
+        self._distance = new_distance
+
+    #Getter
+    @property
+    def resolution(self):
+        return self._resolution
+
+    #Setter
+    @resolution.setter
+    def resolution(self, new_resolution):
+        self._resolution = new_resolution
+
+
