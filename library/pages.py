1d9f29c14549c422bfc5d7c0ba880ba98beacc18
Laura Moser <lauralynn@fullsail.edu>
2015-03-20 00:42:54 -0500

    added results HTML and more

--- library/pages.py
+++ library/pages.py
@@ -8,7 +8,7 @@ Date: 03/18/15
 class Form(object):
     def __init__(self):
         self.css = "css/styles.css"
-        self.__head = '''
+        self.head = '''
 
         <!DOCTYPE HTML>
         <html>
@@ -28,42 +28,42 @@ class Form(object):
 
             <label for="name">Name</label><input type="text" name="name" id="name"/></div>
 
-   <label for="email">Email</label><input type="text" name="email" id="email"/></div>
+            <label for="email">Email</label><input type="text" name="email" id="email"/></div>
 
-    <label for="number">viewing distance</label><input type="number" name="distance" id="distance"/></div>
+            <label for="number">viewing distance</label><input type="number" name="distance" id="distance"/></div>
 
-      <label for="Resolution">Resolution</label>
-      <select id="resolution" name="resolution">
-        <option value="480p">480p</option>
-        <option value="720p">720p</option>
-        <option value="1080p">1080p</option>
-        <option value="4K Ultra">4K Ultra</option>
-      </select>
-
-
-    <input id="submit" type="submit"/><input type="reset" />
-  </form>
-
-
-
+            <label for="Resolution">Resolution</label>
+                <select id="resolution" name="resolution">
+                 <option value="480p">480p</option>
+                 <option value="720p">720p</option>
+                 <option value="1080p">1080p</option>
+                 <option value="4K Ultra">4K Ultra</option>
+             </select>
+             <input id="submit" type="submit"/><input type="reset" />
+            </form>
         '''
-        self.__close = '''
-
+        self.close = '''
                 </body>
             </html>
             '''
 
+        def print_out(self):
+            all = self.head + self.body + self.close
+            all = all.format(**locals())
+            return all
+
 
 class Results(object):
     def __init__(self):
-        self.body = """
+        self.body_two = """
+         <h2>Thank you for your submission!</h2>
+            <p>Please review your results below:</p>
+               <ul>
+                 <li><strong>Minimum TV Size:</strong>{new_min_size}</li>
+                 <li><strong>Maximum TV Size: </strong>{new_max_size}</li>
+                 <li><strong>Resolution Check:</strong>{resolution}</li>
+                <ul>
+            </br>
+            <p>These results were based on your viewing distance of {distance} inches.</p>
 
-
-
-        """
-
-
-
-    def print_out(self):
-        all = self.__head + self.body + self.__close
-        return all
+    """
