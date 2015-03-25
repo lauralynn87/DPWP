bd756535a99058fb040979a225651116505583d7
Laura Moser <lauralynn@fullsail.edu>
2015-03-25 16:28:03 -0500

    Set up the attributes for ContentPage class and set up inherentence from Page

--- dynamic-site/pages.py
+++ dynamic-site/pages.py
@@ -8,16 +8,42 @@ Date: 03/25/15
 
 class Page(object):
     def __init__(self):
-        self.open = """
+        self.css = "css/styles.css"
+        #Meta Tags
+        self.head = """
                 """
-
+        #opening containers go here
         self.body = """
                 """
-
+        #Footer and Closing DIVS
         self.close = """
                 """
 
+    def print_out(self):
+        all = self.head + self.body + self.close
+        all = all.format(**locals())
+        return all
 
+#Will Hold
 class ContentPage(object):
     def __init__(self):
-        pass
+        super(Page, self)._init_()
+
+        #Welcome Section here
+        self.welcome = """
+        """
+
+        #About Me Section Here
+        self.about_me = """
+        """
+
+        #Portfolio Section Here
+        self.portfolio = """
+        """
+
+
+    #Polymorphism - method overriding
+    def print_out(self):
+        all = self.head + self.body + self.welcome + self.about_me + self.portfolio + self.close
+        all = all.format(**locals())
+        return all
