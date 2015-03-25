3d99f9320aa88113e0878bbca59f3af5cbe10b02
Laura Moser <lauralynn@fullsail.edu>
2015-03-25 16:27:10 -0500

    Added the structure to the function code that will be used. Not complete

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -10,7 +10,7 @@ import webapp2
 
 #Necessary Imports
 from pages import Page, ContentPage
-from data import Portfolio
+from data import Portfolio, Data
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -18,7 +18,28 @@ class MainHandler(webapp2.RequestHandler):
         c = ContentPage()
         f = Portfolio()
 
-        #
+        #the if/else function telling the browser what to load based on what the user clicks
+        if  self.request.GET:
+            portolio_filter = Portfolio()
+            portolio_filter = self.request.GET['portfolio_filter']
+
+            if portolio_filter == "show_all":
+                portolio_filter = show_all
+
+            elif portolio_filter == "logos":
+                portolio_filter = logos
+
+            elif portolio_filter == "websites":
+                portolio_filter = websites
+
+            elif portolio_filter == "print-designs":
+                portolio_filter = print_designs
+
+            elif portolio_filter == "package-designs":
+                portolio_filter = package_designs
+
+        else:
+            self.response.write(c.print_out())
 
 
 
