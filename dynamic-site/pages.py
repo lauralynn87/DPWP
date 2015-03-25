78a66c5d0deb922302959c16629b3baa58b8ecb2
Laura Moser <lauralynn@fullsail.edu>
2015-03-25 15:47:47 -0500

    Set up of Page Class and ContentPage Class

--- dynamic-site/pages.py
+++ dynamic-site/pages.py
@@ -4,3 +4,20 @@ Class: Design Patterns for Web Programming
 Assignment: Dynamic Website (Week 4)
 Date: 03/25/15
 '''
+
+
+class Page(object):
+    def __init__(self):
+        self.open = """
+                """
+
+        self.body = """
+                """
+
+        self.close = """
+                """
+
+
+class ContentPage(object):
+    def __init__(self):
+        pass
