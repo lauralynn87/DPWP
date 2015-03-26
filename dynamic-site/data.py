75392c7166396170afccfaa614b18bddc49f1a4f
Laura Moser <lauralynn@fullsail.edu>
2015-03-26 01:21:48 -0500

    updated content in Data page

--- dynamic-site/data.py
+++ dynamic-site/data.py
@@ -35,7 +35,7 @@ class Data(object):
         #Item 1
         show_all.item1 = "images/img-1.jpg"
         show_all.title1 = "Project Title 1"
-        show_all.project1 = "Logo"
+        show_all.project1 = "Logo Design"
         show_all.desc1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 2
         show_all.item2 = "images/img-2.jpg"
@@ -45,7 +45,7 @@ class Data(object):
         #Item 3
         show_all.item3 = "images/img-3.jpg"
         show_all.title3 = "Project Title 3"
-        show_all.project3 = "Print"
+        show_all.project3 = "Print - Business Card"
         show_all.desc3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 
         #Logos Instance
@@ -53,17 +53,17 @@ class Data(object):
          #Item 1
         logos.item1 = "images/img-1.jpg"
         logos.title1 = "Project Title"
-        logos.project1 = "Logo"
+        logos.project1 = "Identity Design"
         logos.desc1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 2
         logos.item2 = "images/img-2.jpg"
         logos.title2 = "Project Title"
-        logos.project2 = "Logo"
+        logos.project2 = "Logo Design"
         logos.desc2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 3
         logos.item3 = "images/img-3.jpg"
         logos.title3 = "Project Title"
-        logos.project3 = "Logo"
+        logos.project3 = "Brand Design"
         logos.desc3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 
 
@@ -72,17 +72,17 @@ class Data(object):
          #Item 1
         websites.item1 = "images/img-1.jpg"
         websites.title1 = "Project Title"
-        websites.project1 = "Logo"
+        websites.project1 = "Responsive Website"
         websites.desc1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 2
         websites.item2 = "images/img-2.jpg"
         websites.title2 = "Project Title"
-        websites.project2 = "Logo"
+        websites.project2 = "Wordpress Website"
         websites.desc2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 3
         websites.item3 = "images/img-3.jpg"
         websites.title3 = "Project Title"
-        websites.project3 = "Logo"
+        websites.project3 = "Simple Website"
         websites.desc3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 
         #Print Designs Instance
@@ -90,17 +90,17 @@ class Data(object):
          #Item 1
         print_designs.item1 = "images/img-1.jpg"
         print_designs.title1 = "Project Title"
-        print_designs.project1 = "Logo"
+        print_designs.project1 = "Poster"
         print_designs.desc1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 2
         print_designs.item2 = "images/img-2.jpg"
         print_designs.title2 = "Project Title"
-        print_designs.project2 = "Logo"
+        print_designs.project2 = "Brochure"
         print_designs.desc2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 3
         print_designs.item3 = "images/img-3.jpg"
         print_designs.title3 = "Project Title"
-        print_designs.project3 = "Logo"
+        print_designs.project3 = "Business Card"
         print_designs.desc3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 
 
@@ -109,17 +109,17 @@ class Data(object):
          #Item 1
         package_designs.item1 = "images/img-1.jpg"
         package_designs.title1 = "Project Title"
-        package_designs.project1 = "Logo"
+        package_designs.project1 = "Package Design"
         package_designs.desc1 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 2
         package_designs.item2 = "images/img-2.jpg"
         package_designs.title2 = "Project Title"
-        package_designs.project2 = "Logo"
+        package_designs.project2 = "Package Design"
         package_designs.desc2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
         #Item 3
         package_designs.item3 = "images/img-3.jpg"
         package_designs.title3 = "Project Title"
-        package_designs.project3 = "Logo"
+        package_designs.project3 = "Package Design"
         package_designs.desc3 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
 
 
