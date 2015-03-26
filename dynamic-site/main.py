3cec380f6710017a9e3de9e95e51c96904e70ee8
Laura Moser <lauralynn@fullsail.edu>
2015-03-26 01:16:40 -0500

    Large Commit - fixed errors, created instance of Data, added code that place the content from data to page

--- dynamic-site/main.py
+++ dynamic-site/main.py
@@ -10,36 +10,93 @@ import webapp2
 
 #Necessary Imports
 from pages import Page, ContentPage
-from data import Portfolio, Filters
+from data import Portfolio, Data
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         p = Page()
         c = ContentPage()
         f = Portfolio()
+        d = Data()
 
         #the if/else function telling the browser what to load based on what the user clicks
         if self.request.GET:
-            portfolio_filter = Portfolio()
-            portfolio_filter = self.request.GET['portfolio-filter']
+            portfolio_filter = self.request.GET['filters']
 
             if portfolio_filter == "show-all":
-                portfolio_filter = show_all
+                c.item1 = d.filters[0].item2
+                c.item2 = d.filters[0].item2
+                c.item3 = d.filters[0].item3
+                c.title1 = d.filters[0].title1
+                c.title2 = d.filters[0].title2
+                c.title3 = d.filters[0].title3
+                c.project1 = d.filters[0].project1
+                c.project2 = d.filters[0].project2
+                c.project2 = d.filters[0].project2
+                c.desc1 = d.filters[0].desc1
+                c.desc2 = d.filters[0].desc2
+                c.desc3 = d.filters[0].desc3
 
             elif portfolio_filter == "logos":
-                portfolio_filter = logos
+                c.item1 = d.filters[1].item1
+                c.item2 = d.filters[1].item2
+                c.item3 = d.filters[1].item3
+                c.title1 = d.filters[1].title1
+                c.title2 = d.filters[1].title2
+                c.title3 = d.filters[1].title3
+                c.project1 = d.filters[1].project1
+                c.project2 = d.filters[1].project2
+                c.project2 = d.filters[1].project2
+                c.desc1 = d.filters[1].desc1
+                c.desc2 = d.filters[1].desc2
+                c.desc3 = d.filters[1].desc3
 
             elif portfolio_filter == "websites":
-                portfolio_filter = websites
+                c.item1 = d.filters[2].item1
+                c.item2 = d.filters[2].item2
+                c.item3 = d.filters[2].item3
+                c.title1 = d.filters[2].title1
+                c.title2 = d.filters[2].title2
+                c.title3 = d.filters[2].title3
+                c.project1 = d.filters[2].project1
+                c.project2 = d.filters[2].project2
+                c.project2 = d.filters[2].project2
+                c.desc1 = d.filters[2].desc1
+                c.desc2 = d.filters[2].desc2
+                c.desc3 = d.filters[2].desc3
 
             elif portfolio_filter == "print-designs":
-                portfolio_filter = print_designs
+                c.item1 = d.filters[3].item1
+                c.item2 = d.filters[3].item2
+                c.item3 = d.filters[3].item3
+                c.title1 = d.filters[3].title1
+                c.title2 = d.filters[3].title2
+                c.title3 = d.filters[3].title3
+                c.project1 = d.filters[3].project1
+                c.project2 = d.filters[3].project2
+                c.project2 = d.filters[3].project2
+                c.desc1 = d.filters[3].desc1
+                c.desc2 = d.filters[3].desc2
+                c.desc3 = d.filters[3].desc3
 
             elif portfolio_filter == "package-designs":
-                portfolio_filter = package_designs
+                c.item1 = d.filters[4].item1
+                c.item2 = d.filters[4].item2
+                c.item3 = d.filters[4].item3
+                c.title1 = d.filters[4].title1
+                c.title2 = d.filters[4].title2
+                c.title3 = d.filters[4].title3
+                c.project1 = d.filters[4].project1
+                c.project2 = d.filters[4].project2
+                c.project2 = d.filters[4].project2
+                c.desc1 = d.filters[4].desc1
+                c.desc2 = d.filters[4].desc2
+                c.desc3 = d.filters[4].desc3
+
+            self.response.write(c.print_out())
 
         else:
-            self.response.write(c.print_out())
+            self.response.write(p.print_out())
 
 
 
